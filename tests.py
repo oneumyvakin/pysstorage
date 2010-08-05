f13ba1d3b1ec80a4d3d4617b72b99a57fedd47d6
flier.lu <flier.lu@ccf670a8-3791-706d-d229-e4249bc3e36d>
2010-08-05 17:13:11 +0000

    support to parse file header

--- tests.py
+++ tests.py
@@ -12,6 +12,11 @@ class TestCompoundFile(unittest.TestCase):
     def testRead(self):
         with CompoundFile('test.doc') as cf:
             self.assertFalse(cf.closed)
+            
+            self.assertEquals(4, cf.major_ver)
+            self.assertEquals(0x3E, cf.minor_ver)
+            self.assertEquals(CompoundFile.DEFAULT_V4_SECTOR_SHIFT, cf.sector_shift)
+            self.assertEquals(CompoundFile.DEFAULT_MINI_SECTOR_SHIFT, cf.mini_sector_shift)
 
 if __name__=='__main__':
     logging.basicConfig(level=logging.DEBUG if "-v" in sys.argv else logging.WARN,
